fa318b88c3acc3fd62e1bff049a9af7f6831d9c4
Any K <yiamy1202@yahoo.com.hk>
2023-08-24 17:58:55 +1200

    Added non-GUI-related tests for extracting file extension

--- src/test/java/TextEditorTest.java
+++ src/test/java/TextEditorTest.java
@@ -1,3 +1,4 @@
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -6,15 +7,84 @@ import static org.junit.Assert.*;
 
 public class TextEditorTest {
 
+//    @Test
+//    void text_default_setting_should_equals_to_config_file() throws IOException {
+//        // load config file directly from the path and check if it's equals to the setting of text area
+//        String validConfigFilePath = "src/main/resources/config.yaml";
+//        TextEditorConfig config = ConfigLoader.loadConfig(validConfigFilePath);
+//        TextEditor textEditor = new TextEditor(config);
+//        assertEquals("Expected Default Font Size", config.getDefaultFontSize(),TextEditor.textArea.getTextArea().getFont().getSize() );
+//        assertEquals("Expected Default Font", config.getDefaultFont(), TextEditor.textArea.getTextArea().getFont().getFontName());
+//    }
+
+    // Tests for extracting different type of files
+
     @Test
-    void text_default_setting_should_equals_to_config_file() throws IOException {
-        // load config file directly from the path and check if it's equals to the setting of text area
-        String validConfigFilePath = "src/main/resources/config.yaml";
-        TextEditorConfig config = ConfigLoader.loadConfig(validConfigFilePath);
+    void txt_file_extension_extraction_should_be_txt() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.txt";
         TextEditor textEditor = new TextEditor(config);
-        assertEquals("Expected Default Font Size", config.getDefaultFontSize(),TextEditor.textArea.getTextArea().getFont().getSize() );
-        assertEquals("Expected Default Font", config.getDefaultFont(), TextEditor.textArea.getTextArea().getFont().getFontName());
+        textEditor.extractFileExtension(fileName);
+        assertEquals("txt", TextEditor.fileExtension);
+    }
+
+    @Test
+    void csv_file_extension_extraction_should_be_csv() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.csv";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("csv", TextEditor.fileExtension);
+    }
+
+    @Test
+    void java_file_extension_extraction_should_be_java() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.java";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("java", TextEditor.fileExtension);
+    }
+
+    @Test
+    void cplusplus_file_extension_extraction_should_be_cpp() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.cpp";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("cpp", TextEditor.fileExtension);
     }
 
 
+    @Test
+    void c_file_extension_extraction_should_be_c() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.c";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("c", TextEditor.fileExtension);
+    }
+    @Test
+    void python_file_extension_extraction_should_be_py() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.py";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("py", TextEditor.fileExtension);
+    }
+    @Test
+    void php_file_extension_extraction_should_be_php() throws IOException {
+        String configFilePath = "src/main/resources/config.yaml";
+        TextEditorConfig config = ConfigLoader.loadConfig(configFilePath);
+        String fileName = "test.php";
+        TextEditor textEditor = new TextEditor(config);
+        textEditor.extractFileExtension(fileName);
+        assertEquals("php", TextEditor.fileExtension);
+    }
 }
